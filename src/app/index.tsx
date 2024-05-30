c8773b34207c2e78159459e7e594ad19ea7fb46f
yoshinari0822 <yoshinari.kanayama0822@gmail.com>
2024-05-30 15:04:56 +0900

    update header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,10 +3,10 @@ import { StyleSheet, Text, View } from "react-native";
 const Index = () => {
   return (
     <View style={styles.container}>
-      <View>
-        <View>
-          <Text>MemoApp</Text>
-          <Text>ログアウト</Text>
+      <View style={styles.header}>
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>MemoApp</Text>
+          <Text style={styles.headerRight}>ログアウト</Text>
         </View>
       </View>
 
@@ -62,8 +62,31 @@ const Index = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+  },
+  header: {
+    backgroundColor: "#466FD3",
+    height: 104,
+    justifyContent: "flex-end",
+  },
+
+  headerInner: {
     alignItems: "center",
+  },
+
+  headerRight: {
+    position: "absolute",
+    right: 16,
+    bottom: 16,
+    color: "#ffffff",
+    opacity: 0.7,
+  },
+
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: "bold",
+    color: "#ffffff",
   },
 });
 
