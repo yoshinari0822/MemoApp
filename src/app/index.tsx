05f645a92210a09d59bf0faeb37c2d1d6d4ef4ff
yoshinari0822 <yoshinari.kanayama0822@gmail.com>
2024-05-30 15:25:30 +0900

    indexの全体のスタイリング

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -13,37 +13,37 @@ const Index = () => {
       {/* メモアイテム一覧 */}
       <View>
         {/* メモ */}
-        <View>
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2023/11/11/ 11:11</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2023/11/11/ 11:11</Text>
           </View>
           <View>
             <Text>X</Text>
           </View>
         </View>
-        <View>
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2023/11/11/ 11:11</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2023/11/11/ 11:11</Text>
           </View>
           <View>
             <Text>X</Text>
           </View>
         </View>
-        <View>
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2023/11/11/ 11:11</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2023/11/11/ 11:11</Text>
           </View>
           <View>
             <Text>X</Text>
           </View>
         </View>
-        <View>
+        <View style={styles.memoListItem}>
           <View>
-            <Text>買い物リスト</Text>
-            <Text>2023/11/11/ 11:11</Text>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.memoListItemDate}>2023/11/11/ 11:11</Text>
           </View>
           <View>
             <Text>X</Text>
@@ -52,8 +52,8 @@ const Index = () => {
       </View>
 
       {/* 追加ボタン */}
-      <View>
-        <Text>＋</Text>
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>＋</Text>
       </View>
     </View>
   );
@@ -87,6 +87,51 @@ const styles = StyleSheet.create({
     lineHeight: 32,
     fontWeight: "bold",
     color: "#ffffff",
+  },
+
+  memoListItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderColor: "rgba(0,0,0,0.15)",
+  },
+
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+
+  memoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: "#848484",
+  },
+
+  circleButton: {
+    position: "absolute",
+    bottom: 40,
+    right: 40,
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: "#367FD3",
+    justifyContent: "center",
+    alignItems: "center",
+    // iOSのみ
+    shadowColor: "#000000",
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 8 },
+    // Androidのみ
+    elevation: 8,
+  },
+
+  circleButtonLabel: {
+    color: "#ffffff",
+    fontSize: 40,
   },
 });
 
