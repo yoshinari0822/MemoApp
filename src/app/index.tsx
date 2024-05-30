16c0d465295316cde0ee1e0a67f4d98dcf3bce43
yoshinari0822 <yoshinari.kanayama0822@gmail.com>
2024-05-30 14:56:51 +0900

    Markup memo list screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,70 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const Index = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <View>
+        <View>
+          <Text>MemoApp</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+
+      {/* メモアイテム一覧 */}
+      <View>
+        {/* メモ */}
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023/11/11/ 11:11</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023/11/11/ 11:11</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023/11/11/ 11:11</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023/11/11/ 11:11</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+
+      {/* 追加ボタン */}
+      <View>
+        <Text>＋</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default Index;
