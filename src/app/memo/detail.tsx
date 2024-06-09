73eab645cd431a7bc5f64abe0667b1b29364e575
yoshinari0822 <yoshinari.kanayama0822@gmail.com>
2024-06-09 17:35:18 +0900

    メモの詳細の取得、スタイルの修正

--- src/app/memo/detail.tsx
+++ src/app/memo/detail.tsx
@@ -1,23 +1,45 @@
-import { router } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
+import { doc, onSnapshot } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { type Memo } from "../../../types/memo";
 import CircleButton from "../../components/CircleButton";
 import Icon from "../../components/Icon";
+import { auth, db } from "../../config";
 
 const handlePress = () => {
   router.push("/memo/edit");
 };
 
 const Detail = () => {
+  const params = useLocalSearchParams();
+  const { id } = params;
+  const [memo, setMemo] = useState<Memo | null>(null);
+
+  useEffect(() => {
+    if (auth.currentUser === null) {
+      return;
+    }
+    const ref = doc(db, `users/${auth.currentUser.uid}/memos`, String(id));
+    const unsubscribe = onSnapshot(ref, (memoDoc) => {
+      const { id, bodyText, updatedAt } = memoDoc.data() as Memo;
+      setMemo({ id: id, bodyText: bodyText, updatedAt: updatedAt });
+    });
+    return unsubscribe;
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.memoHeader}>
-        <Text style={styles.memoTitle}>買い物リスト</Text>
-        <Text style={styles.memoDate}>2023/5/5 10:00</Text>
+        <Text numberOfLines={1} style={styles.memoTitle}>
+          {memo?.bodyText}
+        </Text>
+        <Text style={styles.memoDate}>
+          {memo?.updatedAt.toDate().toLocaleDateString("ja-JP")}
+        </Text>
       </View>
       <ScrollView style={styles.memoBody}>
-        <Text style={styles.memoBodyText}>
-          私の名前は金山義成私の名前は金山義成私の名前は金山義成私の名前は金山義成私の名前は金山義成私の名前は金山義成私の名前は金山義成私の名前は金山義成私の名前は金山義成私の名前は金山義成私の名前は金山義成
-        </Text>
+        <Text style={styles.memoBodyText}>{memo?.bodyText}</Text>
       </ScrollView>
       <CircleButton style={{ top: 60, bottom: "auto" }} onPress={handlePress}>
         <Icon name="pencil" size={40} color="#ffffff" />
@@ -49,10 +71,10 @@ const styles = StyleSheet.create({
     lineHeight: 16,
   },
   memoBody: {
-    paddingVertical: 32,
     paddingHorizontal: 27,
   },
   memoBodyText: {
+    paddingVertical: 32,
     fontSize: 16,
     lineHeight: 24,
   },
